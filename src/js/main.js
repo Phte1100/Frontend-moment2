d106729db67a61d4ccd6365c4756f1a685cb0258
phte1100 <phte1100@student.miun.se>
2024-01-26 09:35:02 +0100

    Klar

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,76 @@
 "use strict";
 
 const url = "https://dahlgren.miun.se/ramschema_ht23.php";
-
+let schedule;
 window.onload = init;
 
 async function init() {
     try {
     const response = await fetch(url);
-    const schedule = await response.json();
+    schedule = await response.json();
 
-    displayCode(code)
+    displaySchedule(schedule)
 
 } catch {
     document.getElementById("error").innerHTML = "<p>Något gick fel!</p>";
 }
 
-function displayCode(schedule) {
-    const codeEl = document.getElementById("table-list");
-
+function displaySchedule(schedule) {
+    const tableEl = document.getElementById("table-list");
+    tableEl.innerHTML = ``;
     schedule.forEach((course) => {
-        codeEl.innerHTML += `
+        tableEl.innerHTML += `
         <tr>
             <td>${course.code}</td>
             <td>${course.coursename}</td>
             <td>${course.progression}</td>
         </tr>
         `;
+        
     });
 }
 
+let codeSort = document.getElementById("code");
+codeSort.addEventListener("click", function (e) {
+    schedule.sort((a, b) => (a.code > b.code) ? 1 : -1);
+    displaySchedule(schedule);
+})
 
-}
+let codeSortReverse = document.getElementById("code");
+codeSortReverse.addEventListener("dblclick", function (e) {
+    schedule.sort((a, b) => (a.code < b.code) ? 1 : -1);
+    displaySchedule(schedule);
+})
+
+let coursenameSort = document.getElementById("name");
+coursenameSort.addEventListener("click", function (e) {
+    schedule.sort((a, b) => (a.coursename > b.coursename) ? 1 : -1);
+    displaySchedule(schedule);
+})
+
+let coursenameSortReverse = document.getElementById("name");
+coursenameSortReverse.addEventListener("dblclick", function (e) {
+    schedule.sort((a, b) => (a.coursename < b.coursename) ? 1 : -1);
+    displaySchedule(schedule);
+})
+
+let progressionSort = document.getElementById("progression");
+progressionSort.addEventListener("click", function (e) {
+    schedule.sort((a, b) => (a.progression > b.progression) ? 1 : -1);
+    displaySchedule(schedule);
+})
+
+let progressionSortReverse = document.getElementById("progression");
+progressionSortReverse.addEventListener("dblclick", function (e) {
+    schedule.sort((a, b) => (a.progression < b.progression) ? 1 : -1);
+    displaySchedule(schedule);
+})
+
+let scheduleFilter = document.getElementById ("search");
+scheduleFilter.addEventListener("input", function (e) {
+    let filtredSchedule = schedule.filter((find) => {
+        return find.code.toLowerCase().includes(e.target.value.toLowerCase())|| find.coursename.toLowerCase().includes(e.target.value.toLowerCase());
+    
+})
+displaySchedule(filtredSchedule);
+})}
